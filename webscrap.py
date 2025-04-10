82f0329cead9f4e55b2644546ffbd85dc054793b
MohamadHoseyn Hashemi <mohamadhoseyn@MohamadHoseyns-MacBook-Air.local>
2025-04-10 20:03:35 +0330

    Added the ability to write the list in a csv file

--- webscrap.py
+++ webscrap.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import time
+import csv
 
 driver = None  # Initialize driver variable
 try:
@@ -16,20 +17,28 @@ try:
     last_height = driver.execute_script("return document.body.scrollHeight")
     while True:
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-        time.sleep(2)
+        time.sleep(1)  # Will not work without!
         new_height = driver.execute_script("return document.body.scrollHeight")
         if new_height == last_height:
             break
         last_height = new_height
+    movies = []
+    # Save and show results
+    with open('movielist.csv','w') as file :
+        writer = csv.writer(file)
 
-    # Parse the page to find the 250 movies
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    movie_list = soup.find('ul', class_="ipc-metadata-list")
-    movies = [h3.get_text(strip=True) for h3 in movie_list.find_all('h3', class_='ipc-title__text')]
+        # Parse the page to find the 250 movies
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        movie_list = soup.find('ul', class_="ipc-metadata-list")
+        for i in movie_list.find_all('h3', class_='ipc-title__text') :
+            i = i.get_text(strip=True)
+            movies.append(i)
+            writer.writerow([i])
 
-    # Show results
-    print(*movies[:], sep="\n")
-    print(f"\nTotal movies scraped: {len(movies)}")  # Should be 250
+        # Show results
+        print(*movies[:], sep="\n")
+        print(f"\nTotal movies scraped: {len(movies)}")  # Should be 250
+        print(time.strftime('%Y-%m-%d %H:%M:%S'))
 
 except Exception as e:
     print("An error occurred: ", e)
